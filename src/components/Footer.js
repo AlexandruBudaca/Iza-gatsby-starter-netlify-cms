a7219444d130b2b9380d5ff970bd967913f754e1
Alex <alex.andrul10@gmail.com>
2020-05-05 18:04:19 +0100

    social

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -74,25 +74,24 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-6 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="vimeo" href="https://vimeo.com">
                   <img
-                    src={facebook}
-                    alt="Facebook"
+                    src={vimeo}
+                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="vimeo" href="https://vimeo.com">
                   <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
+                    src={vimeo}
+                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="vimeo" href="https://vimeo.com">
                   <img
-                    src={instagram}
-                    alt="Instagram"
+                    src={vimeo}
+                    alt="Vimeo"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
